1c4e0461aa3792c2e1bf7a0018c3f637027dbb61
Noa Van den Berghe <vandenberghenoa1@gmail.com>
2024-02-22 13:47:24 +0000

    init

--- index.ts
+++ index.ts
@@ -4,4 +4,4 @@ console.log("Welcome to the JSON data viewer!")
 let choices : string[] = ["View all data", "Filter by ID"];
 
 let index : number = readline.keyInSelect(choices, "Please enter your choice:");
-console.log(`You chose ${choices[index]}.`);e
+console.log(`You chose ${choices[index]}.`);
