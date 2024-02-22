b7500f338964ffb185e10625ca63d74205dc5167
Noa Van den Berghe <vandenberghenoa1@gmail.com>
2024-02-22 13:45:27 +0000

    init

---
+++ index.ts
@@ -0,0 +1,7 @@
+import * as readline from 'readline-sync';
+
+console.log("Welcome to the JSON data viewer!")
+let choices : string[] = ["View all data", "Filter by ID"];
+
+let index : number = readline.keyInSelect(choices, "Please enter your choice:");
+console.log(`You chose ${choices[index]}.`);
