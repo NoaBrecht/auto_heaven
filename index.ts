bd36dcb8cd9e797b61470c13c4620fc55673896b
Noa Van den Berghe <vandenberghenoa1@gmail.com>
2024-02-25 10:16:35 +0000

    fetch

--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import * as readline from 'readline-sync';
 import { Car } from './interfaces';
-let data = fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json').then(response => response.json());
 
-data.then(data=> {
-    let array:Car[] = data;
-})
-const model: Car[] = Array;
-console.log(model)
+let cars: Car[] = [];
+
+fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json')
+    .then(response => response.json())
+    .then((data: Car[]) => {
+        cars = data;
+        cars.forEach(car => console.log(car.id));
+    })
+    .catch(error => console.error('Error:', error));
