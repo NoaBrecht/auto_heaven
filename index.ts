dee959e882c569988fde5fa3fdaca31bce4e843e
Noa Van den Berghe <vandenberghenoa1@gmail.com>
2024-02-26 15:13:35 +0000

    1

--- index.ts
+++ index.ts
@@ -2,10 +2,10 @@ import * as readline from 'readline-sync';
 import { Car } from './interfaces';
 let cars: Car[] = [];
 console.log("Welcome to the JSON data viewer!")
-let choices : string[] = ["View all data", "Filter by ID"];
+let choices: string[] = ["View all data", "Filter by ID"];
 let ID_found: boolean;
 
-let index : number = readline.keyInSelect(choices, "Please enter your choice:");
+let index: number = readline.keyInSelect(choices, "Please enter your choice:");
 fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json')
     .then(response => response.json())
     .then((data: Car[]) => {
@@ -15,7 +15,7 @@ fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.j
             cars.forEach(car => console.log(`- ${car.name} (${car.id})`));
         }
         else if (index == 1) {
-            let Car_ID : string = readline.question("Please enter the ID you want to filter by (CAR-000): ");
+            let Car_ID: string = readline.question("Please enter the ID you want to filter by (CAR-000): ");
             cars.forEach(car => {
                 if (car.id == Car_ID) {
                     console.log(car);
