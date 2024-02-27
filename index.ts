c87465308f1a6956aaafd4390092c125dddcbb74
Noa Van den Berghe <vandenberghenoa1@gmail.com>
2024-02-27 09:54:30 +0000

    code with async

--- index.ts
+++ index.ts
@@ -6,11 +6,33 @@ let choices: string[] = ["View all data", "Filter by ID"];
 let ID_found: boolean;
 
 let index: number = readline.keyInSelect(choices, "Please enter your choice:");
-fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json')
-    .then(response => response.json())
-    .then((data: Car[]) => {
-        cars = data;
-        // cars.forEach(car => console.log(car));
+// fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json')
+//     .then(response => response.json())
+//     .then((data: Car[]) => {
+//         cars = data;
+//         // cars.forEach(car => console.log(car));
+//         if (index == 0) {
+//             cars.forEach(car => console.log(`- ${car.name} (${car.id})`));
+//         }
+//         else if (index == 1) {
+//             let Car_ID: string = readline.question("Please enter the ID you want to filter by (CAR-000): ");
+//             cars.forEach(car => {
+//                 if (car.id == Car_ID) {
+//                     console.log(car);
+//                     ID_found = true;
+//                 }
+//             });
+//             if (!ID_found) {
+//                 console.log(`Er is geen model gevonden met ID: ${Car_ID}`)
+//             }
+//         }
+//     })
+//     .catch(error => console.error('Error:', error));
+
+async function main() {
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json');
+        const cars: Car[] = await response.json();
         if (index == 0) {
             cars.forEach(car => console.log(`- ${car.name} (${car.id})`));
         }
@@ -26,7 +48,9 @@ fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.j
                 console.log(`Er is geen model gevonden met ID: ${Car_ID}`)
             }
         }
-    })
-    .catch(error => console.error('Error:', error));
-
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+main();
 export {}
