1c1a0d24fef9685354de2f4186734a6fdfe4160b
Noa Van den Berghe <vandenberghenoa1@gmail.com>
2024-02-27 09:55:01 +0000

    remove .then code

--- index.ts
+++ index.ts
@@ -4,31 +4,7 @@ let cars: Car[] = [];
 console.log("Welcome to the JSON data viewer!")
 let choices: string[] = ["View all data", "Filter by ID"];
 let ID_found: boolean;
-
 let index: number = readline.keyInSelect(choices, "Please enter your choice:");
-// fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json')
-//     .then(response => response.json())
-//     .then((data: Car[]) => {
-//         cars = data;
-//         // cars.forEach(car => console.log(car));
-//         if (index == 0) {
-//             cars.forEach(car => console.log(`- ${car.name} (${car.id})`));
-//         }
-//         else if (index == 1) {
-//             let Car_ID: string = readline.question("Please enter the ID you want to filter by (CAR-000): ");
-//             cars.forEach(car => {
-//                 if (car.id == Car_ID) {
-//                     console.log(car);
-//                     ID_found = true;
-//                 }
-//             });
-//             if (!ID_found) {
-//                 console.log(`Er is geen model gevonden met ID: ${Car_ID}`)
-//             }
-//         }
-//     })
-//     .catch(error => console.error('Error:', error));
-
 async function main() {
     try {
         const response = await fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json');
