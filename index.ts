995c50c45e802c27600b4334d1788cc45d4cce80
Noa Van den Berghe <vandenberghenoa1@gmail.com>
2024-03-14 13:24:43 +0000

    sort

--- index.ts
+++ index.ts
@@ -13,80 +13,92 @@ app.get('/', (req, res) => {
         });
 })
 app.get('/models', async (req, res) => {
-    let q:string = (typeof req.query.q === "string"?req.query.q:"");
+    let q: string = (typeof req.query.q === "string" ? req.query.q : "");
+    const sortField = typeof req.query.sortField === "string" ? req.query.sortField : "name";
+    const sortDirection = typeof req.query.sortDirection === "string" ? req.query.sortDirection : "asc";
     try {
         const response = await fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json');
         if (response.status === 404) throw new Error('Not found');
         if (response.status === 500) throw new Error('Internal server error');
-        
+
         let cars: Car[] = await response.json();
-        let filteredModes: Car[] = cars.filter((car)=>car.name.toLowerCase().startsWith(q.toLowerCase()));
-        res.render('models',
-        {
-            title: "Models",
-            cars: filteredModes,
-            q:q
+        let filteredModels: Car[] = cars.filter((car) => car.name.toLowerCase().startsWith(q.toLowerCase()));
+        let sortedModels = [...filteredModels].sort((a, b) => {
+            if (sortField === "model") {
+                return sortDirection === "asc" ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name);
+            } else if (sortField === "date") {
+                return sortDirection === "asc" ? new Date(a.date_first_produced).getTime() - new Date(b.date_first_produced).getTime() : new Date(b.date_first_produced).getTime() - new Date(a.date_first_produced).getTime();
+            } else if (sortField === "type") {
+                return sortDirection === "asc" ? a.type.localeCompare(b.type) : b.type.localeCompare(a.type);
+            } else if (sortField === "concept") {
+                return sortDirection === "asc" ? Number(b.concept_car) - Number(a.concept_car) : Number(a.concept_car) - Number(b.concept_car);
+            } else {
+                return 0;
+            }
         });
-        // if (index == 0) {
-        //     cars.forEach(car => console.log(`- ${car.brand.name} - ${car.name} (${car.id})`));
-        // }
-        // else if (index == 1) {
-        //     let Car_ID: string = readline.question("Please enter the ID you want to filter by (CAR-000): ");
-        //     let carsearch: Car | undefined = cars.find((car) => car.id === Car_ID);
-        //     if (carsearch != undefined) {
-        //         console.log(carsearch)
-        //     }
-        //     else {
-        //         console.log(`Er is geen model gevonden met ID: ${Car_ID}`)
-        //     }
-        // }
+        res.render('models',
+            {
+                title: "Models",
+                cars: sortedModels,
+                sortField: sortField,
+                sortDirection: sortDirection,
+                q: q
+            });
     } catch (error) {
         console.error('Error:', error);
     }
 
 })
-app.get('/brands', async(req, res) => {
+app.get('/brands', async (req, res) => {
     try {
         const response = await fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/brands.json');
         if (response.status === 404) throw new Error('Not found');
         if (response.status === 500) throw new Error('Internal server error');
         let brands: Brand[] = await response.json();
         res.render('brands',
-        {
-            title: "Brands",
-            brands: brands
-        });
+            {
+                title: "Brands",
+                brands: brands
+            });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+})
+app.get('/brand/:brandID', async (req, res) => {
+    let ID = req.params.brandID;
+
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/brands.json');
+        if (response.status === 404) throw new Error('Not found');
+        if (response.status === 500) throw new Error('Internal server error');
+        let brands: Brand[] = await response.json();
+        let filteredBrands: Brand[] = brands.filter((brand) => brand.id === ID);
+        res.render('brand',
+            {
+                title: "",
+                brands: filteredBrands
+            });
+    } catch (error) {
+        console.error('Error:', error);
+    }
+})
+app.get('/model/:modelID', async (req, res) => {
+    let ID = req.params.modelID;
+
+    try {
+        const response = await fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json');
+        if (response.status === 404) throw new Error('Not found');
+        if (response.status === 500) throw new Error('Internal server error');
+        let cars: Car[] = await response.json();
+        let filteredModel: Car[] = cars.filter((model) => model.id === ID);
+        res.render('model',
+            {
+                title: "",
+                models: filteredModel
+            });
     } catch (error) {
         console.error('Error:', error);
     }
 })
 
 app.listen(app.get('port'), () => console.log('[server] http://localhost:' + app.get('port')));
-
-// async function main() {
-//     console.log("Welcome to the JSON data viewer!")
-//     // let choices: string[] = ["View all data", "Filter by ID"];
-//     // let index: number = readline.keyInSelect(choices, "Please enter your choice:");
-//     try {
-//         const response = await fetch('https://raw.githubusercontent.com/NoaBrecht/project-web-files/main/cars.json');
-//         if (response.status === 404) throw new Error('Not found');
-//         if (response.status === 500) throw new Error('Internal server error');
-//         const cars: Car[] = await response.json();
-//         // if (index == 0) {
-//         //     cars.forEach(car => console.log(`- ${car.brand.name} - ${car.name} (${car.id})`));
-//         // }
-//         // else if (index == 1) {
-//         //     let Car_ID: string = readline.question("Please enter the ID you want to filter by (CAR-000): ");
-//         //     let carsearch: Car | undefined = cars.find((car) => car.id === Car_ID);
-//         //     if (carsearch != undefined) {
-//         //         console.log(carsearch)
-//         //     }
-//         //     else {
-//         //         console.log(`Er is geen model gevonden met ID: ${Car_ID}`)
-//         //     }
-//         // }
-//     } catch (error) {
-//         console.error('Error:', error);
-//     }
-// }
-// main();
--- interfaces.ts
+++ interfaces.ts
@@ -1,21 +1,21 @@
-export interface Brand{
-    id:string;
-    name:string;
-    country:string;
-    logo:string;
-    foundedYear: number;
-    website:string
+export interface Brand {
+  id: string;
+  name: string;
+  country: string;
+  logo: string;
+  foundedYear: number;
+  website: string
 }
 export interface Car {
-    id: string;
-    name: string;
-    description: string;
-    topspeed: number;
-    concept_car: boolean;
-    date_first_produced: Date;
-    image_url: string;
-    type:string;
-    features:string[];
-    brand: Brand
-  }
-export{}
+  id: string;
+  name: string;
+  description: string;
+  topspeed: number;
+  concept_car: boolean;
+  date_first_produced: Date;
+  image_url: string;
+  type: string;
+  features: string[];
+  brand: Brand
+}
+export { }
